2fd0d578b42b3934c5288074c3213d9a9cc7c926
Eronheit <aquinoeronaldo@gmail.com>
2021-04-12 22:11:03 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,47 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development', 
+  mode: 'development',
   entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, 'public/js'), 
-    publicPath: '/public/js', 
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
     filename: 'bundle.js'
-  }, 
+  },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'], 
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
-  }, 
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
   devServer: {
-    contentBase: './public', 
-    writeToDisk: true, 
+    contentBase: './public',
+    writeToDisk: true,
     historyApiFallback: true
-  }, 
+  },
   externals: {
-    react: 'React', 
+    react: 'React',
     'react-dom': 'ReactDom'
-  }, 
+  },
   plugins: [
     new CleanWebpackPlugin()
   ]
