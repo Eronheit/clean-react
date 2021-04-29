6fee4281183000a3edde8b2a2a293801dc2980be
Eronheit <aquinoeronaldo@gmail.com>
2021-04-29 20:31:46 -0300

    test: ensure MinLengthValidation return falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,3 +8,11 @@ describe('MinLengthValidation', () => {
     expect(error).toEqual(new InvalidFieldError())
   })
 })
+
+describe('MinLengthValidation', () => {
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
+})
