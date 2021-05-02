b036b3e6c8a2e17f09e18a56d1a88300f35222f1
Eronheit <aquinoeronaldo@gmail.com>
2021-05-01 21:20:21 -0300

    fix: typo fieldFieldValidationSpy to fieldValidationSpy

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -3,13 +3,13 @@ import { ValidationComposite } from './validation-composite'
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldFieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldFieldValidationSpy.error = new Error('first_error_message')
-    const fieldFieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldFieldValidationSpy2.error = new Error('second_error_message')
+    const fieldValidationSpy = new FieldValidationSpy('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
+    fieldValidationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
-      fieldFieldValidationSpy,
-      fieldFieldValidationSpy2
+      fieldValidationSpy,
+      fieldValidationSpy2
     ])
     const error = sut.validate('any_field', 'any_value')
     expect(error).toBe('first_error_message')
